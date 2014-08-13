4d38b87c989d7eb6390dd565eff63f6a04d5365c
bobwith5 <bobwith5@gmail.com>
2014-08-13 09:48:15 +0530

    Minor Change

--- DataLabsXC.IntegrationTest/DataLabsXC.IntegrationTest/UnitTest1.cs
+++ DataLabsXC.IntegrationTest/DataLabsXC.IntegrationTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
 
 namespace DataLabsXC.IntegrationTest
 {
@@ -10,15 +11,58 @@ namespace DataLabsXC.IntegrationTest
         public void TestMethod1()
         {
             Class1 class1 = new Class1();
-            string result=class1.MethodA();
+            string result = class1.MethodA();
             Assert.AreEqual("Class1 - MethodA", result);
         }
-         [TestMethod]
+        [TestMethod]
         public void TestMethod2()
         {
             Class1 class1 = new Class1();
-            string result=class1.MethodB();
+            string result = class1.MethodB();
             Assert.AreEqual("Class1 - MethodB", result);
+
+            var doc = new System.Xml.XmlDocument();
+            doc.Load(@"C:\D\UnitTestLog\DataLabsXC.IntegrationTest\DataLabsXC.IntegrationTest\testdefinition\TestDefinitionConfiguration.xml");
+
+            System.Xml.XmlNodeList testNodes = doc.SelectNodes("BuildDefinition/TestDefinitionConfiguration/*");
+            if (testNodes == null)
+            {
+                throw new Exception("No TestDefinitionConfiguration node was found in the XML document.");
+            }
+
+            uint selections = 0;
+            int index = 1;
+            int executeTestCount = 0;
+            bool isFirstRecord = true;
+            var testsToExecute = new StringBuilder();
+            foreach (System.Xml.XmlNode testNode in testNodes)
+            {
+                //uint switchValue = Convert.ToUInt32(testNode.Attributes["Value"].InnerText);
+                //if ((selections & switchValue) != 0)
+                //{
+                executeTestCount++;
+                Console.WriteLine(testNode.Attributes["Name"].InnerText);
+                if (isFirstRecord)
+                {
+                    testsToExecute.Append(index);
+                }
+                else
+                {
+                    testsToExecute.Append(String.Format(",{0}", index));
+                }
+
+                isFirstRecord = false;
+                // }
+
+                index++;
+            }
+
+            if (testNodes.Count == executeTestCount)
+            {
+                string GenerateArtifacts = "true";
+            }
+
+            string TestsToExecute = testsToExecute.ToString();
         }
     }
 }
